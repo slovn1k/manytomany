7284b6467cca443bc5c17f823496603bb5d8cbb1
Unknown <turcanualexandru1992@gmail.com>
2018-02-22 16:47:54 +0200

    CRUD, attach, detach and sync.
    Many to many CRUD app, attach and detach methods and sync method.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,6 +49,10 @@
                 font-size: 84px;
             }
 
+            .title2 {
+                font-size: 64px;
+            }
+
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -88,6 +92,21 @@
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+
+                <div class="title2 m-b-md">
+                    Available commands
+                </div>
+
+                <div class="links">
+                    <a href="/insert_role/{id}/{user_role}">Inserting a new user role</a>
+                    <a href="/read/{id}">Reading certain user roles</a>
+                    <a href="/update/{id}/{user_role}/{new_role}">Update certain user role</a>
+                    <a href="/delete/{id}/{role_id}">Deleting a certain role of a specific user</a>
+                    <br>
+                    <a href="/attach/{id}/{user_role}">Attach a role to a certain user</a>
+                    <a href="/detach/{id}/{user_role}">Detach a role from a certain user</a>
+                    <a href="/sync/{id}">Sinking roles with a user</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,87 @@
 |
 */
 
+use App\User;
+use App\Role;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/insert_role/{id}/{user_role}', function($id, $user_role) {
+    $user = User::findOrFail($id);
+
+    $role = new Role(['role'=>$user_role]);
+
+    if($user->roles()->save($role)){
+        return 'Role has been created successfully';
+    } else {
+        return 'Error while creating a new role!!!';
+    }
+});
+
+Route::get('/read/{id}', function($id) {
+   $user = User::findOrFail($id);
+
+   foreach ($user->roles as $role){
+       echo $role.'<br>';
+
+   }
+
+});
+
+Route::get('/update/{id}/{user_role}/{new_role}', function ($id, $user_role, $new_role){
+   $user = User::findOrFail($id);
+
+   if($user->has('roles')){
+       foreach ($user->roles as $role){
+           if($role->role == $user_role){
+               $role->role = $new_role;
+               if($role->save()){
+                   return 'The role has been modified';
+               } else {
+                   return 'The was an error while updating the role';
+               }
+           }
+       }
+   }
+
+});
+
+Route::get('/delete/{id}/{role_id}', function($id, $role_id){
+    $user = User::findOrFail($id);
+
+    foreach ($user->roles as $role){
+        if($role->whereId($role_id)->delete()){
+            return 'Deleting successful';
+        } else {
+            return 'Error while deleting';
+        }
+    }
+
+});
+
+Route::get('/attach/{id}/{user_role}', function($id, $user_role) {
+    $user = User::findOrFail($id);
+
+    if($user->roles()->attach($user_role)){
+        return 'Attaching a new role to a new user has been successful';
+    } else {
+        return 'Error while attaching the role';
+    }
+});
+
+Route::get('/detach/{id}/{user_role}', function($id, $user_role){
+   $user = User::findOrFail($id);
+
+   $user->roles()->detach($user_role);
+});
+
+
+//Syncing roles with a certain user will attach this roles to the user and remove others is those one don't exist at this user
+Route::get('/sync/{id}', function ($id){
+    $user = User::findOrFail($id);
+
+    $user->roles()->sync([8,9]);
 });
